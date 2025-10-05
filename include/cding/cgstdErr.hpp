fd2256ec5d1666830ca65f16765057e7d71141b0
hunter-hongg <2843249813@qq.com>
2025-10-05 15:43:49 +0800

    增加文档

--- include/cding/cgstdErr.hpp
+++ include/cding/cgstdErr.hpp
@@ -8,9 +8,9 @@ public:
     std::string what() { return errmsg ; }
 protected:
     std::string errmsg;
-};
-class RuntimeErr: public TempErr { };
-class User1Err: public TempErr { };
-class User2Err: public TempErr { };
+}; // 模板错误
+class RuntimeErr: public TempErr { }; // 运行时错误
+class User1Err: public TempErr { }; // 用户错误1
+class User2Err: public TempErr { }; // 用户错误2
 }
 }
